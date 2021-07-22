d48541b4e8c7fae92443ea59ea54fe3c4945cd05
hikarigin99 <hikarigin99@gmail.com>
2021-07-22 20:28:08 +0900

    create 회원service

---
+++ src/main/java/spring/springintroduction/service/MemberService.java
@@ -0,0 +1,35 @@
+package spring.springintroduction.service;
+
+import spring.springintroduction.domain.Member;
+import spring.springintroduction.repository.MemberRepository;
+import spring.springintroduction.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원 가입
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> { // null이 아니라 값이 있으면 동작을 한다. result가 Optional 타입이기 때문에 가능
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+    public List<Member> findMembers() {
+        // 전체 회원 조회
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
