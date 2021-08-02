5c103c3a84084431e8d50f4d481aa36098a4d02a
hikarigin99 <hikarigin99@gmail.com>
2021-08-02 09:59:03 +0900

    스프링 빈 등록 수정 및 의존 관계 세팅

--- src/main/java/spring/springintroduction/SpringConfig.java
+++ src/main/java/spring/springintroduction/SpringConfig.java
@@ -6,10 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import spring.springintroduction.repository.*;
 import spring.springintroduction.service.MemberService;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
@@ -20,24 +16,32 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    //@PersistenceContext 스프링이 알아서 DI 해줌으로 필요 없음
-    private EntityManager em;
+//    //@PersistenceContext 스프링이 알아서 DI 해줌으로 필요 없음
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) { // Injection 받기
+        this.memberRepository = memberRepository;
     }
+
 
     @Bean   // 스프링 빈 등록
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
+        return new MemberService(memberRepository);
+    } // 의존 관계 셋팅
 
-    @Bean   // 스프링 빈 등록
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();    // 인터페이스는 new 안됨, 구현체로 생성
-        //return new JdbcMemberRepository(dataSource);    // 다형성 사용
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean   // 스프링 빈 등록
+//    public MemberRepository memberRepository() {
+//        //return new MemoryMemberRepository();    // 인터페이스는 new 안됨, 구현체로 생성
+//        //return new JdbcMemberRepository(dataSource);    // 다형성 사용
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        //return new JpaMemberRepository(em);
+//    }
 }
